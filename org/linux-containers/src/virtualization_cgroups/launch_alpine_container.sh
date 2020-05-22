f6df0e17e58e846627c252f27611b4298dbc2f5b
Pedro Bruel <pedro.bruel@gmail.com>
2020-05-21 23:11:11 -0300

    Update alpine script

---
+++ org/linux-containers/src/virtualization_cgroups/launch_alpine_container.sh
@@ -0,0 +1,23 @@
+#!/usr/bin/bash
+
+IMG_DIR="alpine_img"
+IMG_REPO="https://us.images.linuxcontainers.org/images"
+IMG_URL="$IMG_REPO/alpine/3.11/amd64/default/20200521_13:00/rootfs.tar.xz"
+[ ! -d $IMG_DIR ] && \
+    mkdir -p $IMG_DIR && \
+    curl $IMG_URL | tar xJ -C $IMG_DIR
+cd $IMG_DIR
+
+CGROUP_ID="MAC0475-145"
+sudo cgcreate -g "cpu,cpuacct,memory:$CGROUP_ID"
+sudo cgset -r cpu.shares=512 "$CGROUP_ID" # 1024 is 100% CPU
+sudo cgset -r memory.limit_in_bytes=10000000000 "$CGROUP_ID"
+
+HOSTNAME="alpine-container"
+sudo cgexec -g "cpu,cpuacct,memory:$CGROUP_ID" \
+     unshare -fmuipn --mount-proc \
+     chroot "$PWD/" \
+     /bin/sh -c "PATH=/bin && mount -t proc proc /proc && hostname $HOSTNAME && sh"
+
+# Cleanup:
+sudo cgdelete "cpu,cpuacct:/$CGROUP_ID"

0a0e376a6439f95e1fe36464df5516733d7e1a69
Pedro Bruel <pedro.bruel@gmail.com>
2020-05-23 01:34:47 -0300

    Update slides

--- org/linux-containers/src/virtualization_cgroups/launch_alpine_container.sh
+++ org/linux-containers/src/virtualization_cgroups/launch_alpine_container.sh
@@ -6,7 +6,6 @@ IMG_URL="$IMG_REPO/alpine/3.11/amd64/default/20200521_13:00/rootfs.tar.xz"
 [ ! -d $IMG_DIR ] && \
     mkdir -p $IMG_DIR && \
     curl $IMG_URL | tar xJ -C $IMG_DIR
-cd $IMG_DIR
 
 CGROUP_ID="MAC0475-145"
 sudo cgcreate -g "cpu,cpuacct,memory:$CGROUP_ID"
@@ -16,7 +15,7 @@ sudo cgset -r memory.limit_in_bytes=10000000000 "$CGROUP_ID"
 HOSTNAME="alpine-container"
 sudo cgexec -g "cpu,cpuacct,memory:$CGROUP_ID" \
      unshare -fmuipn --mount-proc \
-     chroot "$PWD/" \
+     chroot "$IMG_DIR/" \
      /bin/sh -c "PATH=/bin && mount -t proc proc /proc && hostname $HOSTNAME && sh"
 
 sudo cgdelete cpu,cpuacct,memory:/$CGROUP_ID

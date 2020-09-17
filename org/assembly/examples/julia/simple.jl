f1f3aa075611b424404cc79f968e5fcda0def9d8
Pedro Bruel <pedro.bruel@gmail.com>
2020-09-17 13:47:49 -0300

    Update examples

--- org/assembly/examples/julia/simple.jl
+++ org/assembly/examples/julia/simple.jl
@@ -7,6 +7,14 @@ function return15()
     add5(a + 1)
 end
 
+function square(n)
+    n * n
+end
+
+function square_string()
+    square("1")
+end
+
 println("add5, Int64:")
 code_native(add5, (Int64,), syntax = :intel)
 
@@ -15,3 +23,15 @@ code_native(add5, (Float64,), syntax = :intel)
 
 println("return15:")
 code_native(return15, (), syntax = :intel)
+
+# println("square, Int64:")
+# code_native(square, (Int64,), syntax = :intel)
+#
+# println("square, Float64:")
+# code_native(square, (Float64,), syntax = :intel)
+#
+# println("square, String:")
+# code_native(square, (String,), syntax = :intel)
+#
+# println("square_string:")
+# code_native(square_string, (), syntax = :intel)

e131304e66260e82bd6c41639174d2b8628acc77
Pedro Bruel <pedro.bruel@gmail.com>
2020-09-17 14:30:17 -0300

    Update watch script

--- org/assembly/examples/interactive_compilation/square.c
+++ org/assembly/examples/interactive_compilation/square.c
@@ -1,13 +1,14 @@
 #include <stdio.h>
 
-int square(int n){
-    return n * n;
-}
+// int square(int n){
+//     return n * n;
+// }
 
 int main(int argc, char **argv){
     int k = 2;
     int n = 3;
-    // int r = k * k;
+    int r = k * n;
+    r = 2 + 3;
     //int r = square(k);
     return 0;
 }
